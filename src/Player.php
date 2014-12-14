88c712e1eb5f022abcbaf4d190fc9867c0e8a00b
Jacopo Nardiello <jacopo.nardiello@gmail.com>
2014-12-14 20:28:06 +0000

    Player can now move

--- src/Player.php
+++ src/Player.php
@@ -15,10 +15,39 @@ class Player
     public function setPlaceholder($placeholder)
     {
         $this->placeholder = $placeholder;
+        return $this;
+    }
+
+    public function setBoard($board)
+    {
+        $this->board = $board;
+        return $this;
+    }
+
+    public function move($coordString)
+    {
+        $rawCoords = str_split($coordString);
+        $coordMapper = $this->getCoordinatesMapper();
+
+        $coords = [
+            $coordMapper[$rawCoords[0]],
+            $rawCoords[1] - 1
+        ];
+
+        $this->board->set($coords, $this->placeholder);
     }
 
     public function getPlaceholder()
     {
         return $this->placeholder;
+    }
+
+    private function getCoordinatesMapper()
+    {
+        return [
+            'A' => 0,
+            'B' => 1,
+            'C' => 2,
+        ];
     }
 }
---
+++ tests/PlayerTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace TicTacToe;
+
+class PlayerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testPlayerCanMoveOverBoard()
+    {
+        $board = new Board();
+        $john = new Player('John');
+        $john->setPlaceholder('X')
+            ->setBoard($board);
+
+        $al = new Player('Al');
+        $al->setPlaceholder('O')
+            ->setBoard($board);
+
+        $john->move('A1');
+        $al->move('B1');
+        $al->move('B2');
+        $expectedGrid = [
+            ['X', '', ''],
+            ['O', 'O', ''],
+            ['', '', ''],
+        ];
+
+        $this->assertEquals($expectedGrid, $board->toArray());
+    }
+}
