457e16060aed4a90930e6258853b6d6027ee7d65
Jacopo Nardiello <jacopo.nardiello@gmail.com>
2014-12-17 07:58:34 +0000

    Removed duplicated code and testing all row/column/diagonal by DP

--- tests/AiTest.php
+++ tests/AiTest.php
@@ -4,11 +4,36 @@ namespace TicTacToe;
 
 class AiTest extends \PHPUnit_Framework_TestCase
 {
-    public function testAiCanApplyWinRuleByRow()
+    public function coordinatesProvider()
+    {
+        return [
+            [[ // by row
+                [0, 0],
+                [0, 1],
+            ],
+            [0, 2]],
+            [[ // by column
+                [0, 0],
+                [1, 0],
+            ],
+            [2, 0]],
+            [[ // by diagonal
+                [0, 0],
+                [1, 1],
+            ],
+            [2, 2]],
+        ];
+    }
+
+    /**
+     * @dataProvider coordinatesProvider
+     */
+    public function testAiCanApplyWinRuleByRowAndColumnAndDiagonal($fixtures, $expectedCoords)
     {
         $board = new Board();
-        $board->set([0, 0], 'X');
-        $board->set([0, 1], 'X');
+        foreach ($fixtures as $fixture) {
+            $board->set($fixture, 'X');
+        }
 
         $ai = new Ai();
         $ai->setPlaceholder('X')
@@ -17,42 +42,8 @@ class AiTest extends \PHPUnit_Framework_TestCase
         $nextMoveCoords = $ai->deduct();
         $ai->move($nextMoveCoords);
 
-        $thirdCell = $board->get([0, 2]);
+        $thirdCell = $board->get($expectedCoords);
         $this->assertEquals('X', $thirdCell->getValue());
-    }
-
-    public function testAiCanApplyWinRuleByColumn()
-    {
-        $board = new Board();
-        $board->set([0, 0], 'X');
-        $board->set([1, 0], 'X');
-
-        $ai = new Ai();
-        $ai->setPlaceholder('X')
-            ->setBoard($board);
-
-        $nextMoveCoords = $ai->deduct();
-        $ai->move($nextMoveCoords);
-
-        $firstCell = $board->get([2, 0]);
-        $this->assertEquals('X', $firstCell->getValue());
-    }
-
-    public function testAiCanApplyWinRuleByDiagonal()
-    {
-        $board = new Board();
-        $board->set([0, 0], 'X');
-        $board->set([1, 1], 'X');
-
-        $ai = new Ai();
-        $ai->setPlaceholder('X')
-            ->setBoard($board);
-
-        $nextMoveCoords = $ai->deduct();
-        $ai->move($nextMoveCoords);
-
-        $firstCell = $board->get([2, 2]);
-        $this->assertEquals('X', $firstCell->getValue());
     }
 
     public function testNoWinningMoveExists()
