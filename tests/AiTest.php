9ae02941a7e8e9cf65663334cfd1bca10610200c
Jacopo Nardiello <jacopo.nardiello@gmail.com>
2015-01-17 17:52:09 +0000

    Added failing test for missing wrong move from Ai

--- tests/AiTest.php
+++ tests/AiTest.php
@@ -121,6 +121,23 @@ class AiTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($expectedCoords, $nextMoveCoords);
     }
 
+    public function testAiWontMoveIfCreatesForkOpportunityForOpponent()
+    {
+        $board = new Board();
+        $board->set([0, 0], 'X');
+        $board->set([2, 2], 'O');
+        $board->set([1, 1], 'O');
+
+        $ai = new Ai();
+        $ai->setPlaceholder('X')
+            ->setBoard($board);
+
+        $nextMoveCoords = $ai->deduct();
+        $expectedCoords = [0, 2];
+
+        $this->assertEquals($expectedCoords, $nextMoveCoords);
+    }
+
     public function testAiCanMoveToTheCenter()
     {
         $board = new Board();
